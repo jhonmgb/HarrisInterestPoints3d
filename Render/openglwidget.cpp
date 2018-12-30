e824d5bf1e509de56b66efeda59ff85c573b3047
Jhon Mauro Gomez <jhon.mauro@gmail.com>
2018-12-30 19:48:08 +0100

    Issue #21: Bugs and improvements
    Removing unnecessary comments.

--- Render/openglwidget.cpp
+++ Render/openglwidget.cpp
@@ -96,7 +96,6 @@ void OpenGLWidget::paintGL()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
-    //glEnable(GL_CULL_FACE);
 
     shader->bind();
     shader->setUniformValue(locProjection, projection);
@@ -126,7 +125,6 @@ void OpenGLWidget::paintGL()
     }
 
     shader->release();
-    //qDebug() << glGetError() << " ";
 }
 
 /**
